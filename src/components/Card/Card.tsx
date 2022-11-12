e842164a9f2591cb7e1ec1e7bb93ef7f986f5f59
Evgenia Fedorova <leleoartist@gmail.com>
2022-11-12 14:35:55 +0300

    feat: implement skeletons for card images

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react'
+import { Skeleton } from '@mui/material'
+import { FC, useState } from 'react'
 
 import { CourseMainData } from '../../types'
 
@@ -7,14 +8,24 @@ import styles from './style.module.css'
 type Props = { item: CourseMainData }
 
 export const Card: FC<Props> = ({ item }) => {
+  const [loading, setLoading] = useState<boolean>(true)
+
+  const handleLoad = () => setLoading(false)
+
   return (
     <div className={styles.card}>
       <div className={styles.imgWrapper}>
+        {loading && (
+          <Skeleton variant="rectangular" className={styles.skeleton} />
+        )}
         <img
           className={styles.img}
           // src={`url(${item.imgUrl})`}
+          width="100%"
+          height="100%"
           src={`${item.image_link}`}
           alt={item.name}
+          onLoad={handleLoad}
         />
       </div>
       {/* <p className={styles.title}>{item.name}</p>
