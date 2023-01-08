fa5c1b504afc06a9b2094bda18d9c741f34f039c
Evgenia Fedorova <leleoartist@gmail.com>
2023-01-08 15:08:18 +0300

    feat: implement login adaptive

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -77,66 +77,68 @@ export const LoginPage = () => {
   const inputPasswordStyle = classNames(styles.input, styles.inputPassword)
 
   return (
-    <PageWrapper searchHeader={true}>
-      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <img className={styles.logo} src={logo} alt="logo" />
-        <div className={styles.inputs}>
-          <input
-            onFocus={focusHandler}
-            className={styles.input}
-            placeholder="E-mail"
-            {...register('email', {
-              required: 'Введите e-mail',
-              pattern: {
-                value: validEmail,
-                message: 'Введите корректный e-mail',
-              },
-            })}
-          />
-          <p className={styles.error}>
-            {errors.email && <span>{errors.email.message}</span>}
-          </p>
+    <PageWrapper>
+      <div className={styles.wrapper}>
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+          <img className={styles.logo} src={logo} alt="logo" />
+          <div className={styles.inputs}>
+            <input
+              onFocus={focusHandler}
+              className={styles.input}
+              placeholder="E-mail"
+              {...register('email', {
+                required: 'Введите e-mail',
+                pattern: {
+                  value: validEmail,
+                  message: 'Введите корректный e-mail',
+                },
+              })}
+            />
+            <p className={styles.error}>
+              {errors.email && <span>{errors.email.message}</span>}
+            </p>
 
-          <input
-            onFocus={focusHandler}
-            className={inputPasswordStyle}
-            placeholder="Пароль"
-            type="password"
-            {...register('password', {
-              required: 'Введите пароль',
-              minLength: {
-                value: validPasswordLength,
-                message: `Пароль должен быть не менее ${validPasswordLength} символов`,
-              },
-            })}
-          />
-          <p className={styles.error}>
-            {errors.password && <span>{errors.password.message}</span>}
+            <input
+              onFocus={focusHandler}
+              className={inputPasswordStyle}
+              placeholder="Пароль"
+              type="password"
+              {...register('password', {
+                required: 'Введите пароль',
+                minLength: {
+                  value: validPasswordLength,
+                  message: `Пароль должен быть не менее ${validPasswordLength} символов`,
+                },
+              })}
+            />
+            <p className={styles.error}>
+              {errors.password && <span>{errors.password.message}</span>}
+            </p>
+          </div>
+          <p className={classNames(styles.error, styles.back)}>
+            {error && <span>{error}</span>}
           </p>
-        </div>
-        <p className={classNames(styles.error, styles.back)}>
-          {error && <span>{error}</span>}
-        </p>
-        <div className={styles.buttons}>
-          <Button
-            buttonStatus={isBlocked ? 'disabled' : 'normal'}
-            size="xxl"
-            mb="20px"
-            btnType="submit"
-          >
-            Войти
-          </Button>
-          <Button
-            type="outlined"
-            btnType="button"
-            onClick={handleSignUpClick}
-            buttonStatus={isBlocked ? 'disabled' : 'normal'}
-            size="xxl"
-          >
-            Зарегистрироваться
-          </Button>
-        </div>
-      </form>
+          <div className={styles.buttons}>
+            <Button
+              buttonStatus={isBlocked ? 'disabled' : 'normal'}
+              size="xxl"
+              mb="20px"
+              btnType="submit"
+            >
+              Войти
+            </Button>
+            <Button
+              type="outlined"
+              btnType="button"
+              onClick={handleSignUpClick}
+              buttonStatus={isBlocked ? 'disabled' : 'normal'}
+              size="xxl"
+            >
+              Зарегистрироваться
+            </Button>
+          </div>
+        </form>
+      </div>
     </PageWrapper>
   )
 }
